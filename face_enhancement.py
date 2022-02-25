10a855c3b058f56d5e79bef655e9148eb10119c2
柏灌 <baiguan.yt@alibaba-inc.com>
2022-02-25 10:42:41 +0800

    Update weights links

--- face_enhancement.py
+++ face_enhancement.py
@@ -17,13 +17,14 @@ from sr_model.real_esrnet import RealESRNet
 from align_faces import warp_and_crop_face, get_reference_facial_points
 
 class FaceEnhancement(object):
-    def __init__(self, base_dir='./', size=512, model=None, use_sr=True, sr_model=None, channel_multiplier=2, narrow=1, key=None, device='cuda'):
+    def __init__(self, base_dir='./', in_size=512, out_size=512, model=None, use_sr=True, sr_model=None, channel_multiplier=2, narrow=1, key=None, device='cuda'):
         self.facedetector = RetinaFaceDetection(base_dir, device)
-        self.facegan = FaceGAN(base_dir, size, model, channel_multiplier, narrow, key, device=device)
+        self.facegan = FaceGAN(base_dir, in_size, out_size, model, channel_multiplier, narrow, key, device=device)
         self.srmodel =  RealESRNet(base_dir, sr_model, device=device)
         self.faceparser = FaceParse(base_dir, device=device)
         self.use_sr = use_sr
-        self.size = size
+        self.in_size = in_size
+        self.out_size = out_size
         self.threshold = 0.9
 
         # the mask for pasting restored faces back
@@ -42,7 +43,7 @@ class FaceEnhancement(object):
         inner_padding_factor = 0.25
         outer_padding = (0, 0)
         self.reference_5pts = get_reference_facial_points(
-                (self.size, self.size), inner_padding_factor, outer_padding, default_square)
+                (self.in_size, self.in_size), inner_padding_factor, outer_padding, default_square)
 
     def mask_postprocess(self, mask, thres=20):
         mask[:thres, :] = 0; mask[-thres:, :] = 0
@@ -51,7 +52,18 @@ class FaceEnhancement(object):
         mask = cv2.GaussianBlur(mask, (101, 101), 11)
         return mask.astype(np.float32)
 
-    def process(self, img):
+    def process(self, img, aligned=False):
+        orig_faces, enhanced_faces = [], []
+        if aligned:
+            ef = self.facegan.process(img)
+            orig_faces.append(img)
+            enhanced_faces.append(ef)
+
+            if self.use_sr:
+                ef = self.srmodel.process(ef)
+
+            return ef, orig_faces, enhanced_faces
+
         if self.use_sr:
             img_sr = self.srmodel.process(img)
             if img_sr is not None:
@@ -59,7 +71,6 @@ class FaceEnhancement(object):
 
         facebs, landms = self.facedetector.detect(img)
         
-        orig_faces, enhanced_faces = [], []
         height, width = img.shape[:2]
         full_mask = np.zeros((height, width), dtype=np.float32)
         full_img = np.zeros(img.shape, dtype=np.uint8)
@@ -70,7 +81,7 @@ class FaceEnhancement(object):
 
             facial5points = np.reshape(facial5points, (2, 5))
 
-            of, tfm_inv = warp_and_crop_face(img, facial5points, reference_pts=self.reference_5pts, crop_size=(self.size, self.size))
+            of, tfm_inv = warp_and_crop_face(img, facial5points, reference_pts=self.reference_5pts, crop_size=(self.in_size, self.in_size))
             
             # enhance the face
             ef = self.facegan.process(of)
@@ -80,12 +91,14 @@ class FaceEnhancement(object):
             
             #tmp_mask = self.mask
             tmp_mask = self.mask_postprocess(self.faceparser.process(ef)[0]/255.)
-            tmp_mask = cv2.resize(tmp_mask, ef.shape[:2])
+            tmp_mask = cv2.resize(tmp_mask, (self.in_size, self.in_size))
             tmp_mask = cv2.warpAffine(tmp_mask, tfm_inv, (width, height), flags=3)
 
             if min(fh, fw)<100: # gaussian filter for small faces
                 ef = cv2.filter2D(ef, -1, self.kernel)
             
+            if self.in_size!=self.out_size:
+                ef = cv2.resize(ef, (self.in_size, self.in_size))
             tmp_img = cv2.warpAffine(ef, tfm_inv, (width, height), flags=3)
 
             mask = tmp_mask - full_mask
@@ -105,7 +118,8 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, default='GPEN-BFR-512', help='GPEN model')
     parser.add_argument('--key', type=str, default=None, help='key of GPEN model')
-    parser.add_argument('--size', type=int, default=512, help='resolution of GPEN')
+    parser.add_argument('--in_size', type=int, default=512, help='in resolution of GPEN')
+    parser.add_argument('--out_size', type=int, default=512, help='out resolution of GPEN')
     parser.add_argument('--channel_multiplier', type=int, default=2, help='channel multiplier of GPEN')
     parser.add_argument('--narrow', type=float, default=1, help='channel narrow scale')
     parser.add_argument('--use_sr', action='store_true', help='use sr or not')
@@ -121,7 +135,7 @@ if __name__=='__main__':
     
     os.makedirs(args.outdir, exist_ok=True)
 
-    faceenhancer = FaceEnhancement(size=args.size, model=args.model, use_sr=args.use_sr, sr_model=args.sr_model, channel_multiplier=args.channel_multiplier, narrow=args.narrow, key=args.key, device='cuda' if args.use_cuda else 'cpu')
+    faceenhancer = FaceEnhancement(in_size=args.in_size, out_size=args.out_size, model=args.model, use_sr=args.use_sr, sr_model=args.sr_model, channel_multiplier=args.channel_multiplier, narrow=args.narrow, key=args.key, device='cuda' if args.use_cuda else 'cpu')
 
     files = sorted(glob.glob(os.path.join(args.indir, '*.*g')))
     for n, file in enumerate(files[:]):
